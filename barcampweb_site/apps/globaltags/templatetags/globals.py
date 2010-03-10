1e48a131a5631aca0a08c443e4ad2d4a1bda93a6
Horst Gutmann <zerok@zerokspot.com>
2010-03-10 20:39:27 +0100

    Added markdown rendering and form overlays on the proposal page

--- barcampweb_site/apps/globaltags/templatetags/globals.py
+++ barcampweb_site/apps/globaltags/templatetags/globals.py
@@ -4,9 +4,13 @@ globally.
 """
 
 from django.template import Library
-
+from django.contrib.markup.templatetags.markup import markdown as django_markdown
 register = Library()
 
 @register.filter
 def range(int_value):
     return xrange(int_value)
+    
+@register.filter
+def markdown(value):
+    return django_markdown(value, "safe")
