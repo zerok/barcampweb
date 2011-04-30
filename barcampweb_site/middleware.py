a36fdbe02e5677fefcfc37d741609e24603ecaed
Horst Gutmann <zerok@zerokspot.com>
2011-04-30 15:47:58 +0200

    Moved to jquery-mobile

--- barcampweb_site/middleware.py
+++ barcampweb_site/middleware.py
@@ -6,7 +6,7 @@ LOG = logging.getLogger(__name__)
 class PlatformMiddleware(object):
     def process_request(self, request):
         platform = 'default'
-        if 'iphone' in request.get_host():
+        if request.get_host().startswith('m.'):
             platform = 'iphone'
         LOG.debug("Detected platform: %s" % platform)
         setattr(request, 'platform', platform)
